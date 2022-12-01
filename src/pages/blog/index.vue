df45c10ae4345bdbc84a537ea12e37a1b457ded7
Paloma Tejeda <palomatejeda81@gmail.com>
2022-12-01 14:49:06 +0100

    added link to article in blog

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
     <h1>BLOG</h1>
+    <router-link to="/2022-11-15-test-article" class="article-card"
+      ><img src="@/assets/jellyfish.jpg" alt="" class="article-card__img" />
+      <h2 class="article-card__title">Article de test</h2>
+      <h3 class="article-card__description">
+        Catchy description of the article, catchy description
+      </h3></router-link
+    >
   </main>
 </template>
 <script></script>
@@ -12,6 +19,32 @@ main {
   gap: 64px;
   h1 {
     font-family: 'GoodTimes';
+  }
+
+  .article-card {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 300px;
+    border-radius: $radius;
+    background-color: $primary-color-faded;
+    gap: 16px;
+    padding: $mobile-padding;
+
+    &__img {
+      height: 50%;
+      object-fit: cover;
+      border-radius: $radius;
+    }
+    &__title,
+    &__description {
+      text-decoration: none;
+      color: $text-color;
+    }
+
+    &__description {
+      font-weight: $slim-weight;
+    }
   }
 }
 </style>
